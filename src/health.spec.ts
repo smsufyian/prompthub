4c7e67d823fda3e55c50836d3f4fa08f35541243
muhammad.sufyian <s.m.sufyian@gmail.com>
2025-05-20 22:13:03 +0200

    Endpoint to check the liveliness of the application

--- src/health.spec.ts
+++ src/health.spec.ts
@@ -1,7 +1,7 @@
 import {Test, TestingModule} from '@nestjs/testing';
 import {INestApplication} from '@nestjs/common';
 import {AppModule} from './app.module';
-import request from 'supertest';
+import * as request from 'supertest';
 
 describe('Health liveliness check', () => {
     let app: INestApplication;
