bcaa469bbb55700198ae7e0fd8f1baf9f342e497
muhammad.sufyian <s.m.sufyian@gmail.com>
2025-05-20 22:01:15 +0200

    Application should fail health check if endpoint is not exposed

---
+++ src/health.spec.ts
@@ -0,0 +1,27 @@
+import {Test, TestingModule} from '@nestjs/testing';
+import {INestApplication} from '@nestjs/common';
+import {AppModule} from './app.module';
+import request from 'supertest';
+
+describe('Health liveliness check', () => {
+    let app: INestApplication;
+
+    beforeEach(async () => {
+        const moduleFixture: TestingModule = await Test.createTestingModule({
+            imports: [AppModule],
+        }).compile();
+
+        app = moduleFixture.createNestApplication();
+        await app.init();
+    });
+
+    afterEach(async () => {
+        await app.close();
+    });
+
+    it('return 200 when application is live', () => {
+        return request(app.getHttpServer())
+            .get('/health/live')
+            .expect(200);
+    });
+});
